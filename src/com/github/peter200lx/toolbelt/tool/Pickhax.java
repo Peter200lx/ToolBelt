00abba0efe51179e98581b9598c1dba1878915e5
Peter Olson <peter.olson@gmail.com>
2012-03-30 22:14:55 -0700

    Add option range to PickHax

--- src/com/github/peter200lx/toolbelt/tool/Pickhax.java
+++ src/com/github/peter200lx/toolbelt/tool/Pickhax.java
@@ -1,5 +1,6 @@
 package com.github.peter200lx.toolbelt.tool;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -9,7 +10,7 @@ import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.event.block.Action;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.github.peter200lx.toolbelt.Tool;
@@ -23,6 +24,10 @@ public class Pickhax extends Tool  {
 
 	public static String name = "phax";
 
+	private HashMap<String, Long> pCooldown = new HashMap<String, Long>();
+
+	private Integer range;
+
 	@Override
 	public String getToolName() {
 		return name;
@@ -30,27 +35,60 @@ public class Pickhax extends Tool  {
 
 	@Override
 	public void handleInteract(PlayerInteractEvent event){
-		Action act = event.getAction();
-		if(act.equals(Action.LEFT_CLICK_BLOCK) || act.equals(Action.RIGHT_CLICK_BLOCK)) {
-			Block target = event.getClickedBlock();
-			if(target != null && !stopOverwrite.contains(target.getType())       &&
-					(onlyAllow.isEmpty() || onlyAllow.contains(target.getType())) ){
-				if(spawnBuild(target,event.getPlayer())) {
-					Boolean physics;
-					if(act.equals(Action.LEFT_CLICK_BLOCK))
-						physics = true;
-					else
-						physics = false;
-					if(isUseEvent())
-						safeBreak(target,event.getPlayer(),physics);
-					else
-						target.setTypeId(0,physics);
-				}
-			}else if((target != null)&&!target.getType().equals(Material.AIR)) {
-				event.getPlayer().sendMessage(ChatColor.RED + "You can't insta-delete "+
-						ChatColor.GOLD+target.getType());
+		Player subject = event.getPlayer();
+		if(pCooldown.containsKey(subject.getName()) &&
+				(System.currentTimeMillis() < (pCooldown.get(subject.getName())+500)))
+			return;
+		pCooldown.put(subject.getName(), System.currentTimeMillis());
+		Boolean physics = false;
+		Block target;
+		switch(event.getAction()) {
+		case LEFT_CLICK_BLOCK:
+			physics = true;
+		case RIGHT_CLICK_BLOCK:
+			target = event.getClickedBlock();
+			break;
+		case LEFT_CLICK_AIR:
+			physics = true;
+		case RIGHT_CLICK_AIR:
+			if(subject.isSneaking()&&hasRangePerm(subject)&&(range > 0))
+				target = subject.getTargetBlock(null, range);
+			else if(range <= 0){
+				subject.sendMessage(ChatColor.RED+"Ranged block removal isn't enabled");
+				return;
+			}else if(!hasRangePerm(subject)) {
+				subject.sendMessage(ChatColor.RED+"You don't have ranged delete permission");
+				return;
+			}else {
+				subject.sendMessage(ChatColor.RED+"Sorry, didn't catch that, try crouching");
+				return;
 			}
+			break;
+		default:
+			return;
 		}
+		if(target != null && !stopOverwrite.contains(target.getType())       &&
+				(onlyAllow.isEmpty() || onlyAllow.contains(target.getType())) ){
+			if(spawnBuild(target,event.getPlayer())) {
+				if(isUseEvent())
+					if(safeBreak(target,event.getPlayer(),physics))
+						subject.sendBlockChange(target.getLocation(), 0, (byte)0);
+				else {
+					target.setTypeId(0,physics);
+					subject.sendBlockChange(target.getLocation(), 0, (byte)0);
+				}
+			}
+		}else if((target != null)&&!target.getType().equals(Material.AIR)) {
+			event.getPlayer().sendMessage(ChatColor.RED + "You can't insta-delete "+
+					ChatColor.GOLD+target.getType());
+		}
+	}
+
+	private boolean hasRangePerm(CommandSender subject) {
+		if(isPermissions())
+			return subject.hasPermission(getPermStr()+".range");
+		else
+			return true;
 	}
 
 	@Override
@@ -69,6 +107,10 @@ public class Pickhax extends Tool  {
 		//Load the default restriction configuration
 		if(!loadGlobalRestrictions(tSet,conf))
 			return false;
+
+		range = conf.getInt(tSet+"."+name+".range", 25);
+		if(isDebug())
+			log.info("["+modName+"][loadConf] Crouched PickHax range distance is set to "+range);
 
 		List<Integer> intL = conf.getIntegerList(tSet+"."+name+".onlyAllow");
 
